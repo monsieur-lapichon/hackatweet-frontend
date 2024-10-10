43e90650c263850abd46a8c4885ac8fbe939ace2
David Poixblanc <lapichon@gmail.com>
2024-10-10 15:53:26 +0200

    commit

--- components/SignUp.js
+++ components/SignUp.js
@@ -1,17 +1,18 @@
 import styles from '../styles/Home.module.css';
+import Image from 'next/image';
 
 function SignUp () {
     return (
         <div className={styles.signUpContainer}>
             <button className={styles.closeButton}>X</button>
             <div className={styles.logoContainer}>
-                <img className={styles.logo} src="/logo.png" alt="logo"/>
+                <Image src="/logo.png" alt="logo" className={styles.logo} width={100} height={100} />
                 <h3 className={styles.h3}>Create your Hackatweet account</h3>
             </div>
             <form className={styles.formulaire}>
                 <input className={styles.input} type="text" placeholder="firstname"/>
                 <input className={styles.input} type="text" placeholder="username"/>
-                <input className={styles.input} type="text" placeholder="password"/>
+                <input className={styles.input} type="password" placeholder="password"/>
                 <button className={styles.signUpButton} type="submit" >Sign up</button>
             </form>
         </div>
